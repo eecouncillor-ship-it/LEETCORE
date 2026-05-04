84fce5611b95d6336e3b72b1ec8c367a8f38fe82
rahulee24 <rahul24nov2006@gmail.com>
2026-05-04 11:34:55 +0530

    Commit current workspace changes

--- src/app/problems/[id]/page.tsx
+++ src/app/problems/[id]/page.tsx
@@ -85,7 +85,12 @@ export default async function ProblemPage({ params }: ProblemPageProps) {
 
         <section className="space-y-6">
           <div className="rounded-3xl border border-white/10 bg-white/5 p-7 shadow-sm">
-            <SubmissionForm slug={problem.slug} options={problem.options} nextSlug={nextSlug} />
+            <SubmissionForm
+              slug={problem.slug}
+              options={problem.options}
+              nextSlug={nextSlug}
+              isFillInTheBlank={problem.correct_answer === "FIB"}
+            />
           </div>
 
           <div className="rounded-3xl border border-white/10 bg-white/5 p-7 shadow-sm">
